24c022fd8c5d31f60cf602a519fabccc080ff938
milad <msherwani92@gmail.com>
2022-04-30 21:15:44 +0300

    done with ui and validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,173 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const appRun());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class appRun extends StatelessWidget {
+  const appRun({ Key? key }) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: mainScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class mainScreen extends StatefulWidget {
+  
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<mainScreen> createState() => _mainScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _mainScreenState extends State<mainScreen> {
+TextEditingController idController=TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+TextEditingController passController=TextEditingController();
+
+ final formKey= GlobalKey<FormState>();
+
+ bool _showPass=true;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 219, 217, 217),
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor:Color.fromARGB(255, 85, 164, 228) ,
+        title: Text("Sign In"),
+        centerTitle: true,
       ),
+
+
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+                          
+                          SizedBox(
+                            width: 300,
+                            child:  Text("ID", style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),),
+                         SizedBox(
+                         height: 70,
+                         width: 300,
+                         child: TextFormField(
+                         //  initialValue:,
+                           textInputAction: TextInputAction.next,//when press enter automaticlly goto next textField
+                         controller: idController,
+                        
+                          decoration: InputDecoration(
+                            
+                             labelStyle: TextStyle(
+                               
+                color: Color.fromARGB(255, 85, 164, 228),
+                fontSize: 20,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+                          // labelText: "ID",
+                              filled: true,
+                              fillColor:  Color(0xFFFFFFFF),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: BorderSide(color: Colors.black),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: BorderSide(color: Colors.green),
+                            ),
+                             enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: BorderSide(color: Colors.grey[100]!),
+                            ),
+                            hintText: "ID",
+                           
+                          ),
+                           
+                           validator: (value)=>value !=null && value.isEmpty?'Enter Your ID':null,//is null show enter a name
+                         ),
+                       ),    
+        
+
+        SizedBox(
+                            width: 300,
+                            child:  Text("Password", style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),),
+          SizedBox(
+                         height: 70,
+                         width: 300,
+                         child: TextFormField(
+                         //  initialValue:,
+                           textInputAction: TextInputAction.next,//when press enter automaticlly goto next textField
+                         controller: passController,
+                        obscureText: _showPass,
+                        obscuringCharacter: '*',
+                          decoration: InputDecoration( 
+                            
+                             labelStyle: TextStyle(
+                color: Color.fromARGB(255, 85, 164, 228),
+                fontSize: 20,
             ),
-          ],
+                           //labelText: "Password",
+                              filled: true,
+                              fillColor:  Color(0xFFFFFFFF),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: BorderSide(color: Colors.black),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: BorderSide(color: Colors.green),
+                            ),
+                             enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: BorderSide(color: Colors.grey[100]!),
+                            ),
+                            hintText: "Password",
+                           suffixIcon: IconButton(
+                    icon: Icon(
+                        _showPass ? Icons.visibility : Icons.visibility_off),     
+                    onPressed: () {
+                      setState(() {
+                        _showPass = !_showPass;
+                      });
+                    })),
+                          
+                           
+                           validator: (value)=>value !=null && value.isEmpty?'Enter Your Password':null,//is null show enter a name
+                         ),
+                       ),    
+        
+                        SizedBox(height: 30,),
+        
+                        Container(
+                          height: 50,
+                          width: 150,
+                          child: ElevatedButton(onPressed: (){
+                          
+                              final form=formKey.currentState!;
+                                    final isValid=form.validate();//cheeks if our form validated
+                                              
+                                        if(isValid){
+                                    
+                                   
+                                    }else{
+                                 
+                                          }
+                          
+                          }, 
+                          
+                          
+                          child: Text('Sign in',style: TextStyle(fontSize: 20,),),)
+                        ),
+        
+        
+                      ],
+            ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ) ,
     );
   }
 }
